8394173374ff745623004be9270f064a09c7ac9c
TeheronAribam <72928601+TeheronAribam@users.noreply.github.com>
2026-01-01 15:58:11 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (43.27%) - LeetSync

---
+++ Hashing/1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean check(int[] nums) {
+        boolean sorted = true;
+        int smallestChain1 = nums[0];
+        int largestChain2 = nums[nums.length-1];
+        int flips = 0;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] < nums[i-1]) {
+                sorted = false;
+                flips++;
+            }
+        }
+        return sorted ? true : flips > 1 ? false : smallestChain1 >= largestChain2;
+    }
+}

7c433fee0072927d47871b5d3438eb8783da70ff
TeheronAribam <72928601+TeheronAribam@users.noreply.github.com>
2026-01-02 22:21:26 +0530

    Time: 1 ms (45.53%) | Memory: 61.4 MB (40.06%) - LeetSync

--- Arrays/189-rotate-array/rotate-array.java
+++ Arrays/189-rotate-array/rotate-array.java
@@ -1,12 +1,27 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int[] copyNums = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            int newPos = (i + k) % nums.length;
-            copyNums[newPos] = nums[i];
-        }
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = copyNums[i];
+        // int[] copyNums = new int[nums.length];
+        // for (int i = 0; i < nums.length; i++) {
+        //     int newPos = (i + k) % nums.length;
+        //     copyNums[newPos] = nums[i];
+        // }
+        // for (int i = 0; i < nums.length; i++) {
+        //     nums[i] = copyNums[i];
+        // }
+        k = k % nums.length;
+
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+    }
+
+    public static void reverse(int[] nums, int left, int right) {
+        while(left<right) {
+            nums[left] = nums[left] + nums[right];
+            nums[right] = nums[left] - nums[right];
+            nums[left] = nums[left] - nums[right];
+            left++;
+            right--;
         }
     }
 }

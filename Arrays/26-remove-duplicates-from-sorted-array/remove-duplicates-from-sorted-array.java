f5f02bac629f59615873f86b91f36302efd7af96
TeheronAribam <72928601+TeheronAribam@users.noreply.github.com>
2026-01-02 09:31:10 +0530

    Time: 1 ms (73.97%) | Memory: 47 MB (8.27%) - LeetSync

--- Arrays/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ Arrays/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,16 +1,29 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        // int k = 1;
+        // ArrayList<Integer> numsNew = new ArrayList<>();
+        // numsNew.add(nums[0]);
+        // for (int i=1; i<nums.length; i++) {
+        //     if (nums[i] > nums[i-1]) {
+        //         numsNew.add(nums[i]);
+        //         k++;
+        //     }
+        // }
+        // for (int i=0; i<k;i++) {
+        //     nums[i] = numsNew.get(i);
+        // }
+        // return k;
+
         int k = 1;
-        ArrayList<Integer> numsNew = new ArrayList<>();
-        numsNew.add(nums[0]);
-        for (int i=1; i<nums.length; i++) {
-            if (nums[i] > nums[i-1]) {
-                numsNew.add(nums[i]);
+        int lastPointer = 0;
+
+        for (int i = 1; i<nums.length; i++) {
+            if(nums[i] == nums[i-1]) {
+
+            } else {
                 k++;
+                nums[++lastPointer] = nums[i];
             }
-        }
-        for (int i=0; i<k;i++) {
-            nums[i] = numsNew.get(i);
         }
         return k;
     }
